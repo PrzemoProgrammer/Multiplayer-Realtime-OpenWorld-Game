d928202a747f39dfcc1c5b3b8a388ece277d5f9a
PrzemoProgrammer <110130880+PrzemoProgrammer@users.noreply.github.com>
2023-10-14 19:58:37 +0200

    update

--- src/server/db/config/credentials.js
+++ src/server/db/config/credentials.js
@@ -1,6 +1,5 @@
 const USERNAME = process.env.USERNAME;
 const PASSWORD = process.env.PASSWORD;
 const HOST = process.env.HOST;
-// MONGO_CONNECTION_URL=mongodb+srv://ptaku19:<password>@cluster0.f5asglm.mongodb.net/?retryWrites=true&w=majority
-const DB_URL = `mongodb+srv://ptaku19:BFtOqHF6XvKZ2hyJ@cluster0.f5asglm.mongodb.net/?retryWrites=true&w=majority`;
+const DB_URL = ``;
 module.exports = { DB_URL };
